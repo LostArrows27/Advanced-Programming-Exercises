d24551d0302e8f5748eb2d1961a41b23257f5c40
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-03-19 07:52:05 +0700

    Tìm kiếm nhị phân bằng con trỏ

---
+++ 07.Pointers/Binary_search_and_Pointer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(int* arr, int l, int r, int x)
+{
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == x) return m;
+        else if (arr[m] < x) l = m + 1;
+        else r = m - 1;
+    }
+    return -1;
+}
+
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int key;
+    cin >> key;
+    cout << binarySearch(arr, 0, 4, key);
+}
+
+
