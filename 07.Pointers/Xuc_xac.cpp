47efaf5a00f51b15da63b2f46b11580e9ff2f207
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-03-20 15:01:23 +0700

    Roll a dice 2nd version
    Rule: who has the lowest total score will roll first

--- 07.Pointers/Xuc_xac.cpp
+++ 07.Pointers/Xuc_xac.cpp
@@ -6,7 +6,9 @@ void roll_a_dice(int &a);
 
 void updateScore(int &score1, int &score2, const int dice1, const int dice2);
 
-void result(const int &score1, const int &score2, const int dice1, const int dice2);
+void result1(const int &score1, const int &score2, const int dice1, const int dice2);
+
+void result2(const int &score1, const int &score2, const int dice1, const int dice2);
 
 int main()
 {
@@ -14,9 +16,15 @@ int main()
     int score1 = 0, score2 = 0;
     int dice1, dice2;
     while(score1 < 100 && score2 < 100){
-            roll_a_dice(dice1); roll_a_dice(dice2);
-            updateScore(score1, score2, dice1, dice2);
-            result(score1, score2, dice1, dice2);
+            if(score1 <= score2){
+                roll_a_dice(dice1); roll_a_dice(dice2);
+                updateScore(score1, score2, dice1, dice2);
+                result1(score1, score2, dice1, dice2);
+            }else{
+                roll_a_dice(dice2); roll_a_dice(dice1);
+                updateScore(score1, score2, dice1, dice2);
+                result2(score1, score2, dice1, dice2);
+            }
     }
 }
 
@@ -30,8 +38,14 @@ void updateScore(int &score1, int &score2, const int dice1, const int dice2)
     score1 += dice1; score2 += dice2;
 }
 
-void result(const int &score1, const int &score2, const int dice1, const int dice2)
+void result1(const int &score1, const int &score2, const int dice1, const int dice2)
 {
     cout << "xuc xac nguoi choi 1: " << dice1 << "    Tong nguoi 1: " << score1 << endl;
     cout << "xuc xac nguoi choi 2: " << dice2 << "    Tong nguoi 2: " << score2 << endl;
+}
+
+void result2(const int &score1, const int &score2, const int dice1, const int dice2)
+{
+    cout << "xuc xac nguoi choi 2: " << dice2 << "    Tong nguoi 2: " << score2 << endl;
+    cout << "xuc xac nguoi choi 1: " << dice1 << "    Tong nguoi 1: " << score1 << endl;
 }
